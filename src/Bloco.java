77522ffd37ce099671fd7ec6bc5710b9d1503b18
Rogerio Kogawa <rogeriokogawa@gmail.com>
2025-08-25 11:11:57 -0300

    Adicionando construturo da classe Bloco

--- src/Bloco.java
+++ src/Bloco.java
@@ -1,2 +1,17 @@
+import java.util.*;
+
 public class Bloco {
+    private int id;
+    private Map<Integer, Integer> posicaoNumeros;
+    private Map<Integer, Integer> posicaoNumerosIniciais;
+
+    public Bloco(int id, Map<Integer, Integer> posicaoNumerosIniciais){
+        this.id = id;
+        this.posicaoNumerosIniciais = new HashMap<>(posicaoNumerosIniciais);
+        this.posicaoNumeros = new HashMap<>(this.posicaoNumerosIniciais);
+    }
+
+    public boolean adicionarNumero(int numero, int posicaoNumero){
+        
+    }
 }
