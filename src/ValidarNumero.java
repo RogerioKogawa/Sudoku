cb6ead029f415a29b0f2fbb8ddfa8ef9b31e4e59
Rogerio Kogawa <rogeriokogawa@gmail.com>
2025-08-25 11:42:22 -0300

    Adicionando validação de numero entre 1 e 9

--- src/ValidarNumero.java
+++ src/ValidarNumero.java
@@ -1,2 +1,9 @@
-public class ValidarNumero {
+public class ValidarNumero{
+    public  String validar(int numero)throws NumeroInvalidoException{
+        if(numero <= 0 || numero > 9){
+            throw new NumeroInvalidoException("Número inválido, escolha um numero entre 1 e 9");
+        }else{
+            return "Número válido";
+        }
+    }
 }
