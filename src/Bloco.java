bf4497444c099f795d8573c236a119caec31c6ab
Rogerio Kogawa <rogeriokogawa@gmail.com>
2025-08-27 23:03:10 -0300

    Adicionando getters para pegar os valores de uma coluna, e uma linha

--- src/Bloco.java
+++ src/Bloco.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Bloco {
     private int id;
+    //Key = posicao, value = numero na posicao da Key;
     private Map<Integer, Integer> posicaoNumeros;
     private Map<Integer, Integer> posicaoNumerosIniciais;
 
@@ -21,5 +22,44 @@ public class Bloco {
 
     public Map<Integer, Integer> getPosicaoNumerosIniciais() {
         return posicaoNumerosIniciais;
+    }
+
+    public List<Integer> getLinha(int numeroLinha){
+        List<Integer> linha = new ArrayList<>();
+        if(numeroLinha == 0){
+            linha.add(posicaoNumeros.getOrDefault(1, null));
+            linha.add(posicaoNumeros.getOrDefault(2, null));
+            linha.add(posicaoNumeros.getOrDefault(3, null));
+        }
+        if(numeroLinha == 1){
+            linha.add(posicaoNumeros.getOrDefault(4, null));
+            linha.add(posicaoNumeros.getOrDefault(5, null));
+            linha.add(posicaoNumeros.getOrDefault(6, null));
+        }
+        if(numeroLinha == 2){
+            linha.add(posicaoNumeros.getOrDefault(7, null));
+            linha.add(posicaoNumeros.getOrDefault(8, null));
+            linha.add(posicaoNumeros.getOrDefault(9, null));
+        }
+        return linha;
+    }
+
+    public List<Integer> getColuna(int numeroColuna){
+        List<Integer> coluna = new ArrayList<>();
+        if(numeroColuna == 1){
+            coluna.add(posicaoNumeros.getOrDefault(1, null));
+            coluna.add(posicaoNumeros.getOrDefault(4, null));
+            coluna.add(posicaoNumeros.getOrDefault(7, null));
+        }
+        if(numeroColuna == 1){
+            coluna.add(posicaoNumeros.getOrDefault(2, null));
+            coluna.add(posicaoNumeros.getOrDefault(5, null));
+            coluna.add(posicaoNumeros.getOrDefault(8, null));
+        }
+        if(numeroColuna == 1){
+            coluna.add(posicaoNumeros.getOrDefault(3, null));
+            coluna.add(posicaoNumeros.getOrDefault(6, null));
+            coluna.add(posicaoNumeros.getOrDefault(9, null));
+        }
     }
 }
